7fe45ac0b5d322e6e30c0a44c591edc77aeead17
EduardoVaca <vacalalo@gmail.com>
2017-10-24 18:30:58 -0500

    Add attributes class

--- decision_tree.py
+++ decision_tree.py
@@ -1,10 +1,44 @@
+"""
+LAB 5: Decision trees
+Course: AI
+Author: Eduardo Vaca
+"""
+
+class Attribute:
+    
+    def __init__(self, att_str):
+        parts = att_str.split('{')
+        self.name, values_str = parts[0].split(' ')[1], parts[1]
+        for ch in ['}', ' ']:
+            values_str = values_str.replace(ch, '')
+        self.values = values_str.split(',')
+
+    def __str__(self):
+        return '{}: {}'.format(self.name, self.values)
 
 
-
-
-value = input()
-while '@attribute' not in value:
+def read_attributes():
+    """Reads and obtain attributes of the data
+    RETURNS:
+    - dictionary of name->attribute
+    """
     value = input()
-while '@attribute' in value:
-    print(value)
-    value = input()
+    attributes = {}
+    while '@attribute' not in value:
+        value = input()
+    while '@attribute' in value:
+        attribute = Attribute(value)
+        attributes[attribute.name] = attribute
+        value = input()
+    return attributes
+
+def main():
+    """Main program
+    """
+    attributes = read_attributes()
+    for k, v in attributes.items():
+        print('{} -> {}'.format(k, v))
+
+if __name__ == '__main__':
+    main()
+
