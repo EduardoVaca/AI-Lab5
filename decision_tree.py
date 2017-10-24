38ffffd6b38acb6fc39569649471e3e473b4adb4
EduardoVaca <vacalalo@gmail.com>
2017-10-24 18:50:57 -0500

    Add dataset class

--- decision_tree.py
+++ decision_tree.py
@@ -5,7 +5,7 @@ Author: Eduardo Vaca
 """
 
 class Attribute(object):
-    """Class that represents an attribute from the dataset.
+    """Class that represents an attribute from the dataset
     """
 
     def __init__(self, att_str):
@@ -19,20 +19,53 @@ class Attribute(object):
         return '{}: {}'.format(self.name, self.values)
 
 
+class Dataset(object):
+    """Class that represents the dataset tables with list of lists
+    """
+    def __init__(self):
+        self.dataset = []
+
+    def add_data(self, data_str):
+        """Adds new data row to the dataset
+        """
+        self.dataset.append(data_str.split(','))
+
+
 def read_attributes():
     """Reads and obtain attributes of the data
     RETURNS:
-    - dictionary of name->attribute
+    - dictionary of index->attribute
     """
     value = input()
     attributes = {}
     while '@attribute' not in value:
         value = input()
+    count = 0
     while '@attribute' in value:
         attribute = Attribute(value)
-        attributes[attribute.name] = attribute
+        attributes[count] = attribute
+        count += 1
         value = input()
     return attributes
+
+def read_data():
+    """Reads and creates the dataset tables
+    RETURNS:
+    - New dataset
+    """
+    value = input()
+    while '@data' in value:
+        value = input()
+    while '%' in value:
+        value = input()
+    dataset = Dataset()
+    while True:
+        dataset.add_data(value)
+        try:
+            value = input()
+        except EOFError:
+            return dataset
+    return dataset
 
 def main():
     """Main program
@@ -40,6 +73,8 @@ def main():
     attributes = read_attributes()
     for k, v in attributes.items():
         print('{} -> {}'.format(k, v))
+    dataset = read_data()
+    print(dataset.dataset)
 
 if __name__ == '__main__':
     main()
